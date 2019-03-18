b18992a25b87427eae598ea591763433db40019f
apanoo <apanoo@126.com>
2019-03-19 00:13:47 +0800

    fix: fix read resouces function of android

--- core/file.hpp
+++ core/file.hpp
@@ -5,6 +5,16 @@
 class FileUtils {
 public:
     static std::string read( const char* filename ) {
+#ifdef __ANDROID__
+    // android resources path: example.png
+    return read_an(const_del_pre(filename, 7));
+#else
+    // other resources path: assets/example.png
+    return read_noan(filename);
+#endif
+    }
+private:
+    static std::string read_noan(const char* filename) {
         FILE* file = fopen( filename, "rt" );
         if (file == NULL) {
             std::cout << "cannot open file: " << filename << std::endl;
@@ -21,5 +31,39 @@ public:
         std::string result( data );
         delete[] data;
         return result;
+    }
+
+    static std::string read_an(const char* filename) {
+        SDL_RWops *rw = SDL_RWFromFile(filename, "rt");
+        if (rw == NULL) return NULL;
+
+        Sint64 res_size = SDL_RWsize(rw);
+        char* res = (char*)malloc(res_size + 1);
+
+        Sint64 nb_read_total = 0, nb_read = 1;
+        char* buf = res;
+        while (nb_read_total < res_size && nb_read != 0) {
+                nb_read = SDL_RWread(rw, buf, 1, (res_size - nb_read_total));
+                nb_read_total += nb_read;
+                buf += nb_read;
+        }
+        SDL_RWclose(rw);
+        if (nb_read_total != res_size) {
+                free(res);
+                return NULL;
+        }
+
+        res[nb_read_total] = '\0';
+        std::string result(res);
+
+        return result;
+    }
+
+    /**
+     * return target[n-1, ..., sizeof(target)-1]
+     **/
+    static const char* const_del_pre(const char* target, int n) {
+        std::string to(target);
+        return to.substr(n).c_str();
     }
 };
